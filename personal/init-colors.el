e1cf248ba5f27e0d7280a4b2d865bc416d40606e
Brandon Sandrowicz <brandon@sandrowicz.org>
2014-11-25 15:57:07 -0500

    More themes...
    - Adding my own build of the base16 themes (using base16-builder). The
      difference is that I built *all* of the themes, not just a couple.
    - Adding color-theme-ports, a port of the collection of themes that came
      built-in to the color-theme package.

--- personal/init-colors.el
+++ personal/init-colors.el
@@ -1,4 +1,22 @@
 ;;; init-colors.el --- initialize color theme
 
-(load-theme 'base16-railscasts t)
-(load-theme 'solarized-dark t)
+;; Resources
+;; ---------
+;; - http://batsov.com/articles/2012/02/19/color-theming-in-emacs-reloaded/
+;; - http://stackoverflow.com/questions/9900232/changing-color-themes-emacs-24-order-matters
+;; - http://www.emacswiki.org/emacs/?action=browse;oldid=ColorTheme;id=ColorAndCustomThemes
+;; - https://github.com/sellout/emacs-color-theme-solarized/
+
+(let ((paths '("vendor/color-theme-ports"
+               "vendor/base16-themes")))
+     (dolist (element paths)
+       (add-to-list 'custom-theme-load-path
+                    (file-name-as-directory (concat dotfiles-dir element)))))
+
+;; (add-to-list 'custom-theme-load-path
+;;              (file-name-as-directory (concat dotfiles-dir "vendor/color-theme-ports")))
+
+;; (add-to-list 'custom-theme-load-path
+;;              (file-name-as-directory (concat dotfiles-dir "vendor/base16-themes")))
+
+(load-theme 'base16-flat-dark t)
