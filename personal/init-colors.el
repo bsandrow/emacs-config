0c3ed159eab711f3d287e0f138a6d8acc3b0cba7
Brandon Sandrowicz <brandon@sandrowicz.org>
2014-11-25 15:56:07 -0500

    Removing old color-theme stuff

--- personal/init-colors.el
+++ personal/init-colors.el
@@ -1,5 +1,4 @@
 ;;; init-colors.el --- initialize color theme
 
-(require 'color-theme)
-(require 'color-theme-solarized)
-(color-theme-solarized-dark)
+(load-theme 'base16-railscasts t)
+(load-theme 'solarized-dark t)
