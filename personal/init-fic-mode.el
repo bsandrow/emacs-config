8b242f57c1a738507d24d96312554461ada62916
Brandon Sandrowicz <brandon@sandrowicz.org>
2014-11-25 15:59:14 -0500

    Adding fic-mode, but disabling it for now

---
+++ personal/init-fic-mode.el
@@ -0,0 +1,14 @@
+;;; init-fic-mode.el --- Initialize fic-mode options
+
+;; Resources:
+;; * http://www.emacswiki.org/emacs/fic-mode.el
+;; * http://trey-jackson.blogspot.ca/2010/10/emacs-tip-37-fic-modeel.html
+
+;; TODO Need to work on the appearance of highlighting red-on-yellow
+;; bolded and underlined is a bit garrish.
+
+(require 'fic-mode)
+;; (add-hook 'c++-mode-hook 'turn-on-fic-mode)
+;; (add-hook 'emacs-lisp-mode-hook 'turn-on-fic-mode)
+
+;;; init-fic-mode.el end
