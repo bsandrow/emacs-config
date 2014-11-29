553446756fbdb18b071ce4781ec966eebed659b0
Brandon Sandrowicz <brandon@sandrowicz.org>
2014-11-29 14:33:37 -0500

    custom.el: update

--- custom.el
+++ custom.el
@@ -3,13 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#272935" "#da4939" "#a5c261" "#ffc66d" "#6d9cbe" "#b6b3eb" "#6d9cbe" "#f4f1ed"])
- '(ansi-term-color-vector
-   [unspecified "#272935" "#da4939" "#a5c261" "#ffc66d" "#6d9cbe" "#b6b3eb" "#6d9cbe" "#f4f1ed"])
  '(custom-safe-themes
    (quote
-    ("f41fd682a3cd1e16796068a2ca96e82cfd274e58b978156da0acce4d56f2b0d5" default))))
+    ("70c737fc33950d2c9080ad5f7a6121be05608dfa07c31255b42a816d02cf1a0b" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
