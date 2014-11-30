97cab242bf8611719c75af66862781185886f169
Brandon Sandrowicz <brandon@sandrowicz.org>
2014-11-29 22:00:22 -0500

    custom.el: update

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("70c737fc33950d2c9080ad5f7a6121be05608dfa07c31255b42a816d02cf1a0b" default))))
+    ("1bb0d83532cebfa894989bc7c266f80436c151aec971e9c137bb217f61753953" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
