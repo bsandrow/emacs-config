bf126ad74a94e70b1af70a48e326d740ada2aab6
Brandon Sandrowicz <brandon@sandrowicz.org>
2014-12-07 15:10:52 -0500

    Elpa checkin

---
+++ elpa/gitattributes-mode-0.15.0/gitattributes-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "gitattributes-mode" "0.15.0" "Major mode for editing .gitattributes files" 'nil :url "https://github.com/magit/git-modes" :keywords '("convenience" "vc" "git"))
---
+++ elpa/vc-darcs-1.12/vc-darcs-pkg.el
@@ -0,0 +1,1 @@
+(define-package "vc-darcs" "1.12" "a VC backend for darcs" 'nil)
