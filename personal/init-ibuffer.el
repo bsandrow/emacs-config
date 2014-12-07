f82d323aa8ea0a259e07d24baf3a2d2aa89815c9
Brandon Sandrowicz <brandon@sandrowicz.org>
2014-12-07 16:18:03 -0500

    Adding some future-looking stuff. :)

--- personal/init-ibuffer.el
+++ personal/init-ibuffer.el
@@ -11,4 +11,23 @@
             (unless (eq ibuffer-sorting-mode 'alphabetic)
               (ibuffer-do-sort-by-alphabetic))))
 
+;; Source: http://emacs-fu.blogspot.ca/2010/02/dealing-with-many-buffers-ibuffer.html
+;;
+;; (setq ibuffer-saved-filter-groups
+;;       (quote (("default"
+;;                ("Org" (mode . org-mode))
+;;              ))
+;;       )
+;; )
+
+;; Future:
+;;    - Interface to switch between saved filter groups
+;;      - Special-case for ibuffer-vc generated groups
+;;      - Use ido completion
+;;      - Default to ibuffer-vc (since it's auto-generated, it will
+;;        probably apply in more cases).
+;;    - Maybe a way to cycle through all of the saved filter groups (+
+;;      ibuffer-vc). I'm less bullish on this than I am on the ido
+;;      inferface.
+
 ;;; init-ibuffer.el ends here
